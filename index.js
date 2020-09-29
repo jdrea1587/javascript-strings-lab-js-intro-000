3a62139cb67207433313e04097d4dba93ced14b4
Joshua Rea <joshdanielrea@gmail.com>
2020-09-29 05:02:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
-
+"Hello, " + specialGuest + "!" // "Hello, Neil deGrasse Tyson!"
 var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
